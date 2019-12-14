6e37047f0f7eae8f0db6a7f301d3aec45ae20a1d
Hamm <majhamm@qq.com>
2019-12-14 17:55:55 +0800

    add some badges

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="format-detection" content="telephone=no">
+    <style>
+        a {
+            color: white;
+        }
+
+        body,
+        html {
+            padding: 10px;
+            margin: 0px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h2>平台类</h2>
+    <img src="svg/platform/baidu.svg">
+
+    <br>
+    <br>
+    <h2>链接类</h2>
+    <img src="svg/link/hamm.svg">
+
+    <br>
+    <br>
+    <h2>浏览器类</h2>
+    <img src="svg/browser/chrome.svg">
+    <img src="svg/browser/safari.svg">
+
+    <br>
+    <br>
+    <h2>编程语言</h2>
+    <img src="svg/code/php.svg">
+    <img src="svg/code/javascript.svg">
+    <img src="svg/code/node.svg">
+    <img src="svg/code/java.svg">
+    <img src="svg/code/cpp.svg">
+    <img src="svg/code/csharp.svg">
+    <img src="svg/code/python.svg">
+
+    <br>
+    <br>
+    <h2>许可证</h2>
+    <img src="svg/licence/apache-2.0.svg">
+
+
+</body>
+
+</html>
