b926376220257a58e6dc0d4a989a8212afc55430
Hamm <majhamm@qq.com>
2019-12-15 00:38:22 +0800

    add svg tools

--- index.php
+++ index.php
@@ -12,10 +12,6 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="format-detection" content="telephone=no">
     <style>
-        a {
-            color: white;
-        }
-
         body,
         html {
             padding: 10px;
@@ -25,36 +21,86 @@
 </head>
 
 <body>
-
-    <h2>平台类</h2>
-    <img src="svg/platform/baidu.svg">
-
-    <br>
-    <br>
+    <h2>介绍</h2>
+    <h5>本项目是一个快速的svg生成器，方便在外链中快速插入 svg badge.</h5>
+    <h2>调用方式</h2>
+    <h5>1. 直接使用我这边提供的svg生成链接 <a href="https://svg.hamm.cn/?key=Link&value=Hamm.cn">https://svg.hamm.cn/?key=Link&value=Hamm.cn</a></h5>
+    <h5>2. 将项目中的 svg.php 直接部署到你的网站上即可访问。</h5>
+    <h5>3. 调用方式均为  ?key=key&value=value&color=color&radius=radius</h5>
     <h2>链接类</h2>
-    <img src="svg/link/hamm.svg">
+    <img src="https://svg.hamm.cn?key=博客&value=Hamm.cn">
+    <img src="https://svg.hamm.cn?key=百度&value=Baidu.com">
+    <img src="https://svg.hamm.cn?key=谷歌&value=Google.com">
+    <img src="https://svg.hamm.cn?key=Apple&value=www.apple.com">
 
     <br>
     <br>
     <h2>浏览器类</h2>
-    <img src="svg/browser/chrome.svg">
-    <img src="svg/browser/safari.svg">
+    <img src="https://svg.hamm.cn?key=浏览器&value=Chrome">
+    <img src="https://svg.hamm.cn?key=浏览器&value=Safari">
+    <img src="https://svg.hamm.cn?key=浏览器&value=FireFox">
+    <img src="https://svg.hamm.cn?key=浏览器&value=Internet Explore">
+    <img src="https://svg.hamm.cn?key=浏览器&value=Opera">
+    <img src="https://svg.hamm.cn?key=浏览器&value=360极速浏览器">
+
+    <br>
+    <br>
+    <h2>联系方式</h2>
+    <img src="https://svg.hamm.cn?key=Q Q&value=455250574">
+    <img src="https://svg.hamm.cn?key=Wechat&value=majhamm">
+    <img src="https://svg.hamm.cn?key=Email&value=admin@hamm.cn">
+    <img src="https://svg.hamm.cn?key=Weibo&value=800700678">
 
     <br>
     <br>
     <h2>编程语言</h2>
-    <img src="svg/code/php.svg">
-    <img src="svg/code/javascript.svg">
-    <img src="svg/code/node.svg">
-    <img src="svg/code/java.svg">
-    <img src="svg/code/cpp.svg">
-    <img src="svg/code/csharp.svg">
-    <img src="svg/code/python.svg">
+    <img src="https://svg.hamm.cn?key=Language&value=Python">
+    <img src="https://svg.hamm.cn?key=Language&value=Node">
+    <img src="https://svg.hamm.cn?key=Language&value=Javascript">
+    <img src="https://svg.hamm.cn?key=Language&value=C%2b%2b">
+    <img src="https://svg.hamm.cn?key=Language&value=C%23">
+    <img src="https://svg.hamm.cn?key=Language&value=Java">
+    <img src="https://svg.hamm.cn?key=Language&value=PHP">
+    <img src="https://svg.hamm.cn?key=Language&value=Objective-C">
+    <img src="https://svg.hamm.cn?key=Language&value=Golang">
+    <img src="https://svg.hamm.cn?key=Language&value=Swift">
+    <img src="https://svg.hamm.cn?key=Language&value=Lua">
+    <img src="https://svg.hamm.cn?key=Language&value=Shell">
+    <img src="https://svg.hamm.cn?key=Language&value=Dephi">
+    <img src="https://svg.hamm.cn?key=Language&value=Ruby">
+    <img src="https://svg.hamm.cn?key=Language&value=Kotlin">
+    <img src="https://svg.hamm.cn?key=Language&value=HTML">
+    <img src="https://svg.hamm.cn?key=Language&value=CSS">
 
     <br>
     <br>
-    <h2>许可证</h2>
-    <img src="svg/licence/apache-2.0.svg">
+    <h2>编程语言</h2>
+    <img src="https://svg.hamm.cn?key=Runtime&value=Docker">
+    <img src="https://svg.hamm.cn?key=Runtime&value=Linux">
+    <img src="https://svg.hamm.cn?key=Runtime&value=Ubuntu">
+    <img src="https://svg.hamm.cn?key=Runtime&value=Windows">
+    <img src="https://svg.hamm.cn?key=Runtime&value=Mac OS">
+    <img src="https://svg.hamm.cn?key=Runtime&value=Android">
+    <img src="https://svg.hamm.cn?key=Runtime&value=iOS">
+
+    <br>
+    <br>
+    <h2>数据库相关</h2>
+    <img src="https://svg.hamm.cn?key=Database&value=MySQL">
+    <img src="https://svg.hamm.cn?key=Language&value=MSSQL">
+    <img src="https://svg.hamm.cn?key=Language&value=Oracle">
+    <img src="https://svg.hamm.cn?key=Language&value=Access">
+
+    <br>
+    <br>
+    <h2>开源许可证</h2>
+    <img src="https://svg.hamm.cn?key=Licence&value=Apache-2.0">
+    <img src="https://svg.hamm.cn?key=Licence&value=MulanPSL-1.0">
+    <img src="https://svg.hamm.cn?key=Licence&value=0BSD">
+    <img src="https://svg.hamm.cn?key=Licence&value=AGPL-3.0">
+    <img src="https://svg.hamm.cn?key=Licence&value=Artistic-2.0">
+    <img src="https://svg.hamm.cn?key=Licence&value=BSD-2">
+    <img src="https://svg.hamm.cn?key=Licence&value=BSD-3">
 
 
 </body>
---
+++ svg.php
@@ -0,0 +1,86 @@
+<?php
+    header('Content-Type:image/svg+xml');
+    $key = trim($_GET['key'] ?? 'Key');
+    $value = trim($_GET['value'] ?? 'Value');
+    
+    $len_key = 11 * mb_strlen($key);
+    if(preg_match('/[a-zA-Z]$/', $key)){
+        $len_key = 6.5 * strlen($key);
+    }
+    
+    $len_value = 11 * mb_strlen($value);
+    if(preg_match('/[a-zA-Z.+-_#*\/@]$/', $value)){
+        $len_value = 6.5 * strlen($value);
+    }
+    
+    $radius = $_GET['radius'] ?? 3;
+    
+    $backColor = '#ff4500';
+    switch(strtolower($value)){
+        case 'php':
+            $backColor = '#6c7fd5';
+            $len_value = 9 * strlen($value);
+            break;
+        case 'c++':
+            $backColor = '#074680';
+            $len_value = 8.5 * strlen($value);
+            break;
+        case 'c#':
+            $backColor = '#662577';
+            $len_value = 8.5 * strlen($value);
+            break;
+        case 'java':
+            $backColor = '#b85916';
+            break;
+        case 'javascript':
+            $backColor = '#69a86b';
+            break;
+        case 'node':
+            $backColor = '#6da149';
+            $len_value = 8 * strlen($value);
+            break;
+        case 'node':
+            $backColor = '#6da149';
+            $len_value = 8 * strlen($value);
+            break;
+        case 'python':
+            $backColor = '#fccd4d';
+            break;
+        default:
+            $backColor = !empty($_GET['color']) ? "#".$_GET['color'] : randColor();
+    }
+    $len_total = $len_key+$len_value + 11*2;
+   
+function randColor() 
+{ 
+  $str='3456789ABC'; 
+    $estr='#'; 
+    $len=strlen($str); 
+    for($i=1;$i<=6;$i++) 
+    { 
+        $num=rand(0,$len-1);   
+        $estr=$estr.$str[$num];  
+    } 
+    return $estr; 
+} 
+?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="<?php echo $len_total;?>" height="20">
+    <linearGradient id="b" x2="0" y2="100%">
+        <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+        <stop offset="1" stop-opacity=".1"/>
+    </linearGradient>
+    <clipPath id="a">
+        <rect width="<?php echo $len_total;?>" height="20" rx="<?php echo $radius;?>" fill="#fff"/>
+    </clipPath>
+    <g clip-path="url(#a)">
+        <path fill="#333" d="M0 0h<?php echo $len_key+11;?>v20H0z"/>
+        <path fill="<?php echo $backColor;?>" d="M<?php echo $len_key+11;?> 0h<?php echo $len_value+11;?>v20H<?php echo $len_key+11;?>z"/>
+        <path fill="url(#b)" d="M0 0h<?php echo $len_total;?>v20H0z"/>
+    </g>
+    <g fill="#fff" text-anchor="middle" font-family="Source Code Pro" font-size="110">
+        <text x="<?php echo $len_key*5+55;?>" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="<?php echo $len_key*10;?>"><?php echo $key;?></text>
+        <text x="<?php echo $len_key*5+55;?>" y="140" transform="scale(.1)" textLength="<?php echo $len_key*10;?>"><?php echo $key;?></text>
+        <text x="<?php echo $len_value*5+ 110*1.5 + $len_key*10;?>" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="<?php echo $len_value*10;?>"><?php echo $value;?></text>
+        <text x="<?php echo $len_value*5+ 110*1.5 + $len_key*10;?>" y="140" transform="scale(.1)" textLength="<?php echo $len_value*10;?>"><?php echo $value;?></text>
+    </g>
+</svg>
