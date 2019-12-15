fe84055ff71688d558c92c6a506b895244cd0730
Hamm <majhamm@qq.com>
2019-12-15 22:23:25 +0800

    update svg.php.

--- svg.php
+++ svg.php
@@ -1,5 +1,9 @@
 <?php
     header('Content-Type:image/svg+xml');
+    if(empty($_GET['key']) || empty($_GET['value'])){
+        header('Location: https://gitee.com/hamm/svg_badge_tool');
+        die;
+    }
     $key = trim($_GET['key'] ?? 'Key');
     $value = trim($_GET['value'] ?? 'Value');
     
