041aa4110fd1af3a4b9c8429cb0c7834139c00f2
Hamm <majhamm@qq.com>
2020-03-29 16:51:24 +0800

    添加客户端相关数据接口

--- svg.php
+++ svg.php
@@ -6,8 +6,27 @@
     }
     $key = trim($_GET['key'] ?? 'Key');
     $value = trim($_GET['value'] ?? 'Value');
-    
-    switch($key){
+    switch(strtolower($key)){
+        case 'client':
+            $value=strtolower($value);
+            switch($value){
+                case 'ip':
+                    $key = "IP";
+                    $value=get_client_ip();
+                    break;
+                case 'os':
+                    $key = "系统";
+                    $value=getOs();
+                    break;
+                case 'broswer':
+                    $key = "浏览器";
+                    $value=getBrowser();
+                    break;
+                default:
+                    $key="Client";
+                    $value="Tools";
+            }
+            break;
         case 'gitee':
             $key = ucfirst($key);
             switch($value){
@@ -184,6 +203,94 @@ function httpGetFull($url, $header = [], $cookies = "", $returnHeader = false, $
     }
     curl_close($ch);
     return $output;
+}
+
+function get_client_ip()
+{
+    foreach (array(
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_X_FORWARDED',
+        'HTTP_X_CLUSTER_CLIENT_IP',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_FORWARDED',
+        'REMOTE_ADDR'
+    ) as $key) {
+        if (array_key_exists($key, $_SERVER)) {
+            foreach (explode(',', $_SERVER[$key]) as $ip) {
+                $ip = trim($ip);
+                if ((bool) filter_var(
+                    $ip,
+                    FILTER_VALIDATE_IP,
+                    FILTER_FLAG_IPV4
+                    // FILTER_FLAG_NO_PRIV_RANGE |
+                    // FILTER_FLAG_NO_RES_RANGE
+                )) {
+                    return $ip;
+                }
+            }
+        }
+    }
+    return null;
+}
+
+/**
+ * 获取操作系统
+ *
+ * @return string
+ */
+function  getOs()
+{
+    if (empty($_SERVER['HTTP_USER_AGENT'])) {
+        return 'Other';
+    }
+    $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+    if (strpos($agent, 'windows nt')) {
+        $platform = 'Windows';
+    } elseif (strpos($agent, 'macintosh')) {
+        $platform = 'MacOS';
+    } elseif (strpos($agent, 'ipod')) {
+        $platform = 'iPod';
+    } elseif (strpos($agent, 'ipad')) {
+        $platform = 'iPad';
+    } elseif (strpos($agent, 'iphone')) {
+        $platform = 'iPhone';
+    } elseif (strpos($agent, 'android')) {
+        $platform = 'Android';
+    } elseif (strpos($agent, 'unix')) {
+        $platform = 'Unix';
+    } elseif (strpos($agent, 'linux')) {
+        $platform = 'Linux';
+    } else {
+        $platform = 'Other';
+    }
+    return $platform;
+}
+/**
+ * 获取浏览器
+ *
+ * @return void
+ */
+function  getBrowser()
+{
+    if (empty($_SERVER['HTTP_USER_AGENT'])) {
+        return 'Unknown';
+    }
+    $agent = $_SERVER["HTTP_USER_AGENT"];
+    if (strpos($agent, 'MSIE') !== false || strpos($agent, 'rv:11.0')) //ie11判断
+    {
+        return "IE";
+    } else if (strpos($agent, 'Firefox') !== false) {
+        return "Firefox";
+    } else if (strpos($agent, 'Chrome') !== false) {
+        return "Chrome";
+    } else if (strpos($agent, 'Opera') !== false) {
+        return 'Opera';
+    } else if ((strpos($agent, 'Chrome') == false) && strpos($agent, 'Safari') !== false) {
+        return 'Safari';
+    } else {
+        return 'Unknown';
+    }
 }
 ?>
 <!-- This is build by svg tool , see more here :  https://gitee.com/hamm/svg_badge_tool-->
