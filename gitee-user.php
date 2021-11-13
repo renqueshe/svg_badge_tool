05fdf754d3cbe5b85938c4d38078a88f5d291cc2
Hamm <majhamm@qq.com>
2021-11-13 17:35:18 +0800

    fix: 修改显示字体

--- gitee-user.php
+++ gitee-user.php
@@ -30,42 +30,42 @@ $userHead = "data:image/jpeg;base64," . base64_encode(httpGetFull($user['avatar_
 
     <rect x="50" y="50" width="400" height="250" rx="10" ry="10" style="fill:url(#black_gray);" />
     
-    <text x="250" y="140" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['name'];?></text>
-    <text x="250" y="141" fill="#fff" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['name'];?></text>
+    <text x="250" y="140" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['name'];?></text>
+    <text x="250" y="141" fill="#fff" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['name'];?></text>
     
     
-    <text x="250" y="160" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="16" style='dominant-baseline:middle;text-anchor:middle;'>@<?php echo $user['login'];?></text>
-    <text x="250" y="161" fill="#999" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="16" style='dominant-baseline:middle;text-anchor:middle;'>@<?php echo $user['login'];?></text>
+    <text x="250" y="160" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="16" style='dominant-baseline:middle;text-anchor:middle;'>@<?php echo $user['login'];?></text>
+    <text x="250" y="161" fill="#999" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="16" style='dominant-baseline:middle;text-anchor:middle;'>@<?php echo $user['login'];?></text>
     
     
-    <text x="250" y="200" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['bio'];?></text>
-    <text x="250" y="201" fill="#999" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['bio'];?></text>
+    <text x="250" y="200" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['bio'];?></text>
+    <text x="250" y="201" fill="#999" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['bio'];?></text>
     
     
-    <text x="110" y="260" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>开源</text>
-    <text x="110" y="261" fill="#999" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>开源</text>
-    <text x="110" y="240" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['public_repos'];?></text>
-    <text x="110" y="241" fill="#fff" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['public_repos'];?></text>
+    <text x="110" y="260" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>开源</text>
+    <text x="110" y="261" fill="#999" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>开源</text>
+    <text x="110" y="240" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['public_repos'];?></text>
+    <text x="110" y="241" fill="#fff" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['public_repos'];?></text>
     
-    <text x="180" y="260" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>粉丝</text>
-    <text x="180" y="261" fill="#999" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>粉丝</text>
-    <text x="180" y="240" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['followers'];?></text>
-    <text x="180" y="241" fill="#fff" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['followers'];?></text>
+    <text x="180" y="260" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>粉丝</text>
+    <text x="180" y="261" fill="#999" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>粉丝</text>
+    <text x="180" y="240" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['followers'];?></text>
+    <text x="180" y="241" fill="#fff" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['followers'];?></text>
     
-    <text x="250" y="260" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>关注</text>
-    <text x="250" y="261" fill="#999" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>关注</text>
-    <text x="250" y="240" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['following'];?></text>
-    <text x="250" y="241" fill="#fff" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['following'];?></text>
+    <text x="250" y="260" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>关注</text>
+    <text x="250" y="261" fill="#999" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>关注</text>
+    <text x="250" y="240" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['following'];?></text>
+    <text x="250" y="241" fill="#fff" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['following'];?></text>
     
-    <text x="320" y="260" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Stars</text>
-    <text x="320" y="261" fill="#999" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Stars</text>
-    <text x="320" y="240" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['stared'];?></text>
-    <text x="320" y="241" fill="#fff" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['stared'];?></text>
+    <text x="320" y="260" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Stars</text>
+    <text x="320" y="261" fill="#999" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Stars</text>
+    <text x="320" y="240" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['stared'];?></text>
+    <text x="320" y="241" fill="#fff" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['stared'];?></text>
     
-    <text x="390" y="260" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Watches</text>
-    <text x="390" y="261" fill="#999" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Watches</text>
-    <text x="390" y="240" fill="#000" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['watched'];?></text>
-    <text x="390" y="241" fill="#fff" font-family="Consolas, PingFangSC-Regular, Microsoft YaHei" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['watched'];?></text>
+    <text x="390" y="260" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Watches</text>
+    <text x="390" y="261" fill="#999" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="14" style='dominant-baseline:middle;text-anchor:middle;'>Watches</text>
+    <text x="390" y="240" fill="#000" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['watched'];?></text>
+    <text x="390" y="241" fill="#fff" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" font-size="24" style='dominant-baseline:middle;text-anchor:middle;'><?php echo $user['watched'];?></text>
     
     <rect x="195" y="0" width="110" height="110" rx="110" ry="110" style="fill:#fff;stroke-width:5;"  />
     <rect id="headImageRect" x="200" y="5" width="100" height="100" rx="100" ry="100" style="fill:#eee;stroke-width:5;"  />
